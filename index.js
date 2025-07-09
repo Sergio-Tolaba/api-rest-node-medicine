39cceb46e3456877482a6e1fa521207456d58af1
Sergio-Tolaba <tolser2019@gmail.com>
2025-07-09 13:48:17 -0300

    install firebase and dotenv, index with get '/' first route - ok postman

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+import "dotenv/config"
 import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+app.get('/', (req,res)=>{
+    res.json({message: "API Rest Medicine with Node.js"})
+})
+
+const PORT = process.env.PORT ||3001;
 
 app.listen(PORT,()=> console.log(`Server running on http://localhost:${PORT}`))
 
